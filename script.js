6fe9346417a55d3dd30426e9b74569d06537d5d6
Eli Allen <65967638+mojojoflow@users.noreply.github.com>
2023-07-05 22:25:06 -0500

    Add core features, work in progress
    I'm unclear on the exact event needed to draw on the grid without
    clicking each individual square. 'click' & 'mousedown' only seems to
    work on one square at a time. While 'mouseover' works but without
    clicking at all.

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
 const gridContainer = document.querySelector('.grid-container');
 const createGridBtn = document.querySelector('#create-grid');
+let squareColor = "black";
+
+function promptForDimensions() {
+  const invalid = [NaN, 0];
+
+  do {
+    let input = prompt("Please enter a grid size.  Max Limit: 100.");
+    let dimension = Number(input);
+
+    if (invalid.includes(dimension) || dimension > 100 || input === null) {
+      alert(`Invalid Input: ${input} .Please try again.`);
+    } else {
+      return dimension;
+    }
+  } while (true);
+}
+
+
+function createGridSquares() {
+
+  let dimension = promptForDimensions();
+
+  if (gridContainer.innerHTML !== '') {
+    gridContainer.innerHTML = '';
+  }
+
+  updateGridDimensions(dimension);
+  const totalSquares = dimension ** 2;
+
+  for (let i = 0; i < totalSquares; i++) {
+    let newSquare = document.createElement("div");
+    newSquare.addEventListener('mousedown', function () {
+      this.style.backgroundColor = squareColor;
+    });
+    gridContainer.appendChild(newSquare);
+  }
+
+}
+
+function updateGridDimensions(dimension) {
+  gridContainer.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+}
+
+createGridBtn.addEventListener('click', createGridSquares);
